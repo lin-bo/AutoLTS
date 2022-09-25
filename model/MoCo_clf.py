5d93846835fe26c992983bd23e1993cd0e5298c9
lin-bo <imbo.lin@mail.utoronto.ca>
2022-09-25 16:40:15 -0400

    add models back

---
+++ model/MoCo_clf.py
@@ -0,0 +1,33 @@
+import torch
+import torchvision
+from torch import nn
+
+
+class MoCoClf(nn.Module):
+
+    def __init__(self, checkpoint_name=None, local=True):
+        super(MoCoClf, self).__init__()
+        # initialize the model
+        model = torchvision.models.resnet50(pretrained=True)
+        for name, param in model.named_parameters():
+            # if name not in {'fc.weight', 'fc_bias'}:
+            param.requires_grad = False
+        # # load the checkpoint
+        if local:
+            checkpoint = torch.load(f'./checkpoint/{checkpoint_name}.pt', map_location='cpu')
+        else:
+            checkpoint = torch.load(f'./checkpoint/{checkpoint_name}.pt')
+        state_dict = checkpoint['model_state_dict']
+        for k in list(state_dict.keys()):
+            if k.startswith('encoder_q') and not k.startswith('encoder_q.fc'):
+                state_dict[k[len('encoder_q.'):]] = state_dict[k]
+            del state_dict[k]
+        _ = model.load_state_dict(state_dict, strict=False)
+        # # add FC layers
+        dim = model.fc.weight.shape[0]
+        model = nn.Sequential(model, nn.Linear(dim, 4), nn.Softmax(dim=-1))
+        self.clf = model
+
+    def forward(self, x):
+        return self.clf(x)
+
